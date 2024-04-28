3591bb9d06427edbed63d44df5694c7153da2fbc
moonlight01010 <ansharrajulul@gmail.com>
2024-04-28 16:35:40 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var button =document.getElementById("button");
+
+button.addEventListener("click", function(){
+    alert("Hello, nice to see u here!");
+    alert("don't expect to much this is just for fun!!");
+});
